8b0f4092ea50e34b2b629fe09269abd7b3b63d0e
Michael Dowling <mtdowling@gmail.com>
2014-04-08 00:11:47 -0700

    Fixing method call

--- lib/jmespath.lua
+++ lib/jmespath.lua
@@ -2,7 +2,7 @@
 
 local Parser = require("parser")
 local interpreter = require("interpreter")
-local parser = Parser:create()
+local parser = Parser:new()
 
 -- Returns a function that can search hashes using JMESPath expressions.
 return function (expression, data)
