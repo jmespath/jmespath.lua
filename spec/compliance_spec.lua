8b8a76cc6be9ee26ebf85baceceba422d68a6ff5
Michael Dowling <mtdowling@gmail.com>
2014-04-09 17:07:56 -0700

    Cleaning up compliance test

--- spec/compliance_spec.lua
+++ spec/compliance_spec.lua
@@ -15,36 +15,36 @@ describe('compliance', function()
 
   -- Run a test suite
   local function runsuite(file)
-    print(file)
     data = assert(load_test(file))
 
     for i, suite in ipairs(data) do
       for i2, case in ipairs(suite.cases) do
-        local result = jmespath.search(case.expression, suite.given)
-        print(file .. ": " .. i .. ": " .. i2 .. ": " .. case.expression)
-        if case.result == nil then
-          assert.is_nil(result)
-        else
-          assert.are.same(case.result, result)
-        end
+        local name = string.format("%s from %s (Suite %s, case %s)",
+          case.expression, file, i, i2)
+        it(name, function()
+          local result = jmespath.search(case.expression, suite.given)
+          if case.result == nil then
+            assert.is_nil(result)
+          else
+            assert.are.same(case.result, result)
+          end
+        end)
       end
     end
   end
 
-  it("passes compliance tests", function()
-    local prefix = lfs.currentdir() .. "/spec/compliance/"
-    local iter, obj = lfs.dir(prefix)
-    local cur = obj:next()
+  local prefix = lfs.currentdir() .. "/spec/compliance/"
+  local iter, obj = lfs.dir(prefix)
+  local cur = obj:next()
 
-    while cur do
-      local attr = lfs.attributes(prefix .. cur)
-      if attr.mode == "file" then
-        runsuite(prefix .. cur) 
-      end
-      cur = obj:next()
+  while cur do
+    local attr = lfs.attributes(prefix .. cur)
+    if attr.mode == "file" then
+      runsuite(prefix .. cur)
     end
+    cur = obj:next()
+  end
 
-    obj:close()
-  end)
+  obj:close()
 
 end)
